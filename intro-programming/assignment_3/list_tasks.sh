eadf6955a114f861e8f39f532ee54d71f89acfef
mishka28 <mishiko28_chigo@yahoo.com>
2017-06-13 22:07:39 -0400

    commiting creating the class

---
+++ intro-programming/assignment_3/list_tasks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f ~/task_database.txt ]; then
+	cat ~/tast_database.txt
+fi
+
+
+
---
+++ intro-programming/assignment_3/save_task.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+newTask="Do 30 Push Ups"
+echo $newTask >> ~/task_database.txt
+
+
