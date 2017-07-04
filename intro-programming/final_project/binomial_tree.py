13bd6f3459401631b9d8cd925e9084f4f7681ac3
mishka28 <mishiko28_chigo@yahoo.com>
2017-07-04 12:39:39 -0400

    final project is finished and running

--- intro-programming/final_project/binomial_tree.py
+++ intro-programming/final_project/binomial_tree.py
@@ -3,36 +3,34 @@
 from datetime import date
 import math
 import numpy as np
+import sys
 
 today = date(2015, 11, 20)
 expiry = date(2017, 1, 20)
 
 #Global inputs
-
-flavor = "p"    #put or call ? 
 s = 138.65
 k = 133
 t = (expiry - today).days / 365
 r = 0.0106161830925
 sig = 0.22545538993
 div = .027000
-steps = 10
-
-Dt = t / steps
-r_Dt = r * Dt
-exp_r_Dt = math.exp(r_Dt)
-exp_neg_r_Dt = math.exp(-r_Dt)
-exp_div_Dt = math.exp(div * Dt)
-exp_neg_div_Dt = math.exp(-div * Dt)
-sig_sqrt_Dt = sig * math.sqrt(Dt)
-exp_sig_sqrt_Dt = math.exp(sig_sqrt_Dt)
-
-u = exp_sig_sqrt_Dt
-d = 1/u
-q = (exp_r_Dt * exp_neg_div_Dt - d) / (u -d)
-p = 1 - q
 
 def tree(steps, flavor):
+    Dt = t / steps
+    r_Dt = r * Dt
+    exp_r_Dt = math.exp(r_Dt)
+    exp_neg_r_Dt = math.exp(-r_Dt)
+    exp_div_Dt = math.exp(div * Dt)
+    exp_neg_div_Dt = math.exp(-div * Dt)
+    sig_sqrt_Dt = sig * math.sqrt(Dt)
+    exp_sig_sqrt_Dt = math.exp(sig_sqrt_Dt)
+
+    u = exp_sig_sqrt_Dt
+    d = 1/u
+    q = (exp_r_Dt * exp_neg_div_Dt - d) / (u -d)
+    p = 1 - q
+
     sv = np.zeros((steps + 1,steps +1), dtype = np.float64)
     sv[0,0] = s
     z1 = 0 
@@ -44,10 +42,20 @@ def tree(steps, flavor):
     iv = np.zeros((steps + 1,steps +1), dtype = np.float64)
     z2 = 0
     for i in range(1,steps + 1, 1):
-        for n in range(z2 + 1):
-            iv[n,i] = max(sv[n,i] - k, 0)
+        find = False
+        for n in range(z2 + 2):
+            if flavor == "C":
+                iv[n,i] = max(sv[n,i] - k, 0)
+            elif flavor == "P":
+                iv[n,i] = max(-1 * (sv[n,i] - k), 0)
+            else:
+                print("fravor has to be 'C' for call and 'P' for put")
+                find = True
+                break
+        if find:
+            break
         z2 = z2 + 1
-
+    
     pv = np.zeros((steps + 1,steps +1), dtype = np.float64)
     pv[:, steps] = iv[:, steps]
     z3 = steps + 1
@@ -55,17 +63,16 @@ def tree(steps, flavor):
         z3 = z3 - 1
         for n in range(z3):
             pv[n,i] = (q * pv[n, i + 1] + p * pv[n + 1, i + 1]) * exp_neg_r_Dt
-    return(pv)
-#    return(pv[0,0])
+    return(pv[0,0])
+
+if __name__ == "__main__":
+    steps = int(sys.argv[1])
+    flavor = (sys.argv[2])    
+    print(tree(steps,flavor))  
 
 
-#    return(sv)
-#    return(iv)
 
 
 
-print(tree(9,12))
-
-#  for i in reversed(range(steps)):
 
 
